28c17d1132ff4eb09e4efcaa59898e4a467d8fb6
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-07-01 00:42:39 +0300

    2 Done logic for open selected point of menu

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
+import os
+import time
+
 #======================func=============================#
 
 def print_menu(ind_pos = 0):
     print("\nmenu:")
     for i in range(len(arr_menu)):
         if i == ind_pos:
-            print("->\t", arr_menu[i])
+            print("-> ", arr_menu[i])
             continue
-        print("\t", arr_menu[i])
+        print("   ", arr_menu[i])
 
-def check_pos(ind_pos = 0):
+def receive_pos(ind_pos = 0):
     while True:
-        direct = input("enter \"w\" / \"s\" -> ")
+        direct = input("   ")
 
         # increment & decrement
-        if direct == 'W' or direct == 'w':
+        if not direct:
+            return ind_pos, direct
+        elif direct == 'W' or direct == 'w':
             ind_pos += 1
         elif direct == 'S' or direct == 's':
             ind_pos -= 1
         else:
             print("ERROR!")
             continue
+            # return ind_pos
 
         # check position
         if ind_pos < 0:
@@ -27,15 +33,36 @@ def check_pos(ind_pos = 0):
         elif ind_pos > 2:
             ind_pos = 0
 
-        return ind_pos
+        return ind_pos, direct
+def new_game():
+    print("\n -> New game")
+
+def set_game():
+    print("\n -> Settings")
+
+def exit_game():
+    print("\n -> Exit")
+    return True
 
 #======================main=============================#
 
 arr_menu = ["New game", "Settings", "Exit"]
 ind = 0
 
+menu_f = {
+    0: new_game,
+    1: set_game,
+    2: exit_game
+}
 print("\n\"w\" - move Down\n\"s\" - move Up")
 
 while True:
     print_menu(ind)
-    ind = check_pos(ind)
+    ind, operation = receive_pos(ind)
+    time.sleep(0)
+    os.system('CLS')
+
+    if not operation:
+        logic = menu_f[ind]()
+        if type(logic) == bool and logic:
+            break
